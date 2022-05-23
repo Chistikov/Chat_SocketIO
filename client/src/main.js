13e6ca22eb74a8ff9f863c96a43a72e577e85698
VladChistikov <vlad.chistikov998@gmail.com>
2022-05-23 23:12:39 +0300

    Added .env to client

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import socket from './plugins/socketIO'
 
+const { VUE_APP_SERVER_URL } = process.env;
+alert(VUE_APP_SERVER_URL)
+
 createApp(App)
   .use(router)
-  .use(socket, {serverURL: "http://localhost:3000/"})
+  .use(socket, {serverURL: VUE_APP_SERVER_URL})
   .mount('#app')
